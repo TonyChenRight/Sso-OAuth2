a125809bdeee03a9c5fb7e9572d44ab2a7f60e66
TonyChenRight <chenjun_tony@126.com>
2018-12-05 00:15:26 +0800

    搭建sso单点登录骨架

---
+++ sso-server/src/main/java/com/huobi/SsoApplication.java
@@ -0,0 +1,14 @@
+package com.huobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import sun.applet.Main;
+
+@SpringBootApplication
+public class SsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class);
+    }
+}
